ad4c15fd29ec23aac32c95a46e75343dc96ad709
priceymineral <tudaeva@gmail.com>
2023-01-13 17:19:24 -0800

    completed code-challenges.test.js

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,11 +13,12 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+// TEST PSEUDO
 // create test for codify fxn
 // describe 
 // it changes the following characters: "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 // the actual result should equal the expected
-// check for good error 
+// check for 'good' error (<function name> is not defined)
 
 describe('codify', () => {
     it('it changes "a" to 4, "e" to 3, "i" to 1, and "o" to 0', () => {
@@ -34,6 +35,7 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+// FXN PSEUDO
 // initialize codify functino which takes one string input
 // iterate over the string 
 // replace each letter accordingly
@@ -51,6 +53,18 @@ const codify = (str) => {
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+// TEST PSEUDO
+// intialize a test for a function called targetwords
+// describe 
+// it should return an array with only the words containing the target string
+// expect the actual output to equal the expected
+// check for 'good' error (<function name> is not defined)
+describe('targetWords', () => {
+    it('returns an array with the words in the input array containing the target string only', () => {
+        expect(targetWords(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(targetWords(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -60,10 +74,46 @@ const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+// FXN PSEUDO
+// create target words fxn that takes in an array and a target string
+// initialize a new target array
+// iterate over array
+// check if current string contains target string (capital or lower case)
+// if it does, push to new array
+// return new array
+
+const targetWords = (words, target) => {
+    let targetWords = []
+    target = target.toLowerCase()
+    // let newTarget = new RegExp(target, "g") # unsuccessful attempt to use RegEx, received TypeError below
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].toLowerCase().includes(target)) {
+        // if (words[i].includes(newTarget)) { // TypeError: First argument to String.prototype.includes must not be a regular expression
+        // at String.includes (<anonymous>)
+            targetWords.push(words[i])
+        }
+    }
+
+    return targetWords
+}
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+// TEST PSEUDO
+// describe isFullHouse fxn 
+// it determines if the input array is a full house 
+// expect the actual input to equal the expected
+// check for 'good' error (<function name> is not defined)
+describe('isFullHouse', () => {
+    it('determines if input array holds a full house', () => {
+        expect(isFullHouse(hand1)).toEqual(true)
+        expect(isFullHouse(hand2)).toEqual(false)
+        expect(isFullHouse(hand3)).toEqual(false)
+        expect(isFullHouse(hand4)).toEqual(true)
+    })
+})
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -75,3 +125,75 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+// FXN PSEUDO
+// initialize isFullHouse to take one input array
+// initialize a map
+// iterate over hand
+// if current number is not in map
+    // add it 
+    // if count incremented > 2
+    // return false
+// if it is in map
+    // increment the map value
+
+// if map.values array includes a 3 or a 2
+    // return true
+// else 
+    // return false
+
+const isFullHouse = (hand) => {
+    if (hand.length < 5) return false
+
+    let counts = {}
+    let quit = 0
+
+    // for (let i = 0; i < hand.length; i++) {
+    //     if (counts[hand[i]]) {
+    //         ++counts[hand[i]]
+    //     } else {
+    //         if (++quit > 2) return false
+    //         counts[hand[i]] = 1
+    //     }
+    // }
+    
+    // for...of loop answer
+    for (const card of hand) {
+        if (counts[card]) {
+            ++counts[card]
+        } else {
+            if (++quit > 2) return false
+            counts[card] = 1
+        }
+    }
+
+    if (Object.values(counts).includes(2)) return true
+
+    return false
+}
+// Attempt to do challenge 3 using Map()
+    // let counts = new Map() 
+    // let quit = 0
+    // for (let i = 0; i < hand.length; i++) {
+    //     if (counts.get(hand[i])) {
+    //         counts.set(hand[i], counts.get(hand[i])+1)
+            
+    //     } else {
+    //         counts.set(hand[i], 1)
+    //         if(quit++ > 2) {
+    //             return false
+    //         }
+    //     }
+    // }
+
+    // console.log('counts => ', counts.values())
+    // let vals = counts.values()
+    // vals.forEach(num => { // TypeError: vals.forEach is not a function
+    //     if (num === 2 || num === 3) {
+    //         return true
+    //     }
+//     })
+
+//     return false 
+//     // if (counts.values().includes(3) || counts.values().includes(2)) {
+//     //     return true
+//     // }
